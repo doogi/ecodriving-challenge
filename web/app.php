6543e6ae58b5362f646fd38627e2c851b35e3b64
Piotr Zygmuntowicz <piotr.zygmuntowicz@itaka.pl>
2016-09-17 21:07:26 +0200

    dla Damiana

--- web/app.php
+++ web/app.php
@@ -190,58 +190,125 @@ $app->get('/trip', function () use ($app, $m) {
         );
 });
 
-$app->get('/blog', function () use ($app) {
-    $m = new MongoDB\Client("mongodb://localhost:27017");
-
+$app->get('/trips', function () use ($app, $m) {
     $data = [];
 
-    foreach ($m->data->rows->find() as $row) {
+
+    $speedList = [];
+    $rpmList = [];
+    foreach ($m->data->rows->find([],['sort' => ['recorded_at' => 1]]) as $row) {
         if (empty($row['loc'])) {
             continue;
+        }
+
+        $points = 0;
+        $violations = [];
+        $obedience = [];
+        if (!empty($row['fields']['BEHAVE_ID']) && $row['fields']['BEHAVE_ID']['b64_value'] === ACCELERATION_VIOLATION) {
+            $points += 10;
+            $beginingSpeed = calculateKpH(decodeInt($row['fields']['BEHAVE_GPS_SPEED_BEGIN']['b64_value'])['val']);
+            $endingSpeed = calculateKpH(decodeInt($row['fields']['BEHAVE_GPS_SPEED_END']['b64_value'])['val']);
+            $duration = decodeInt($row['fields']['BEHAVE_ELAPSED']['b64_value'])['val']/1000;
+            $obedience[] = [
+                'id' => 'LIGHT_SPEED_OBEDIENCE',
+                'desc' => sprintf('You accelerated from %d km/h to %d km/h in %.2f seconds, now try to keep the speed!', $beginingSpeed, $endingSpeed, $duration)
+            ];
+        }
+
+        if (!empty($row['fields']['BEHAVE_ID']) && $row['fields']['BEHAVE_ID']['b64_value'] === BREAKING_VIOLATION) {
+            $points -= 10;
+            $beginingSpeed = calculateKpH(decodeInt($row['fields']['BEHAVE_GPS_SPEED_BEGIN']['b64_value'])['val']);
+            $endingSpeed = calculateKpH(decodeInt($row['fields']['BEHAVE_GPS_SPEED_END']['b64_value'])['val']);
+            $duration = decodeInt($row['fields']['BEHAVE_ELAPSED']['b64_value'])['val']/1000;
+            $violations[] = [
+                'id' => 'HARD_BREAKS_VIOLATION',
+                'desc' => sprintf('You decelerated from %d km/h to %d km/h in %.2f seconds!', $beginingSpeed, $endingSpeed, $duration)
+            ];
+        }
+
+        if (!empty($row['fields']['MDI_OBD_RPM'])) {
+            $rpm = decodeInt($row['fields']['MDI_OBD_RPM']['b64_value'])['val'];
+            if (!empty($rpm)) {
+                $rpmList[] = $rpm;
+                if ($rpm > 2000) {
+                    $points -= 10;
+                    $violations[] = [
+                        'id' => 'RPM_VIOLATION',
+                        'desc' => sprintf('Whoa! You reached %d RPMs, fuel is disappearing like in black hole!', $rpm)
+                    ];
+                }
+            }
+        }
+
+        if (!empty($row['fields']['MDI_OBD_SPEED'])) {
+            $speed = decodeInt($row['fields']['MDI_OBD_SPEED']['b64_value'])['val'];
+            if (!empty($speed)) {
+                $speedList[] = $speed;
+                if ($speed > 130) {
+                    $speedList = [];
+                    $points -= 10;
+                    $violations[] = [
+                        'id' => 'OVERSPEED_VIOLATION',
+                        'desc' => sprintf('Hey Bandit, keep calm and slow down a little, you\'re not in a plane, %d km/h is too much', $speed)
+                    ];
+                }
+            }
         }
 
         $data[] = [
             'lng' => $row['loc'][0],
             'lat' => $row['loc'][1],
-            'timestamp' => strtotime($row['recorded_at'])
+            'timestamp' => strtotime($row['recorded_at']),
+            'points' => $points,
+            'violations' => $violations,
+            'obedience' => $obedience
         ];
     }
 
-//    $accelerations = [ ];
-//    foreach ($m->data->rows->find(['fields.BEHAVE_ID.b64_value' => $accelerationId]) as $row) {
-//        $speedAtBeginning = calculateKpH(decodeValue($row['fields']['BEHAVE_GPS_SPEED_BEGIN']['b64_value'])['val']);
-//        $speedAtEnd = calculateKpH(decodeValue($row['fields']['BEHAVE_GPS_SPEED_END']['b64_value'])['val']);
-//        $duration = decodeValue($row['fields']['BEHAVE_ELAPSED']['b64_value'])['val'];
-//        $accelerations[] = [
-//            'speedOnStart' => $speedAtBeginning,
-//            'speedOnEnd' => $speedAtEnd,
-//            'duration' => $duration,
-//            'acceleration' => caclulateAcceleration($speedAtBeginning, $speedAtEnd, $duration/1000)
-//        ];
-//    };
-//
-//    $breakings = [ ];
-//    foreach ($m->data->rows->find(['fields.BEHAVE_ID.b64_value' => $breakingID]) as $row) {
-//        $speedAtBeginning = calculateKpH(decodeValue($row['fields']['BEHAVE_GPS_SPEED_BEGIN']['b64_value'])['val']);
-//        $speedAtEnd = calculateKpH(decodeValue($row['fields']['BEHAVE_GPS_SPEED_END']['b64_value'])['val']);
-//        $duration = decodeValue($row['fields']['BEHAVE_ELAPSED']['b64_value'])['val'];
-//        $breakings[] = [
-//            'speedOnStart' => $speedAtBeginning,
-//            'speedOnEnd' => $speedAtEnd,
-//            'duration' => $duration,
-//            'acceleration' => caclulateAcceleration($speedAtBeginning, $speedAtEnd, $duration/1000)
-//        ];
-//    };
-//
-//    $data = [ ];
-//    $data['accelerations'] = $accelerations;
-//    $data['breakings'] = $breakings;
+    if (count($speedList) > 5) {
+        $standardDeviation = sd($speedList);
+        $lastEntry = end($data);
+
+        $averageSpeed = array_sum($speedList)/count($speedList);
+        if ($standardDeviation < 10 && $averageSpeed < 100) {
+            $data[] = [
+                'lng' => $lastEntry['lng'],
+                'lat' => $lastEntry['lat'],
+                'timestamp' => $lastEntry['timestamp'],
+                'points' => 10,
+                'violations' => [],
+                'obedience' => [
+                    'id' => 'EQUAL_SPEED_OBEDIENCE',
+                    'desc' => sprintf('You kept your speed (%.2f km/h) for a while, good job!', $averageSpeed)
+                ]
+            ];
+        }
+    }
+
+    if (count($rpmList) > 3) {
+        $standardDeviation = sd($rpmList);
+        $lastEntry = end($data);
+
+        $averageRpm = array_sum($rpmList)/count($speedList);
+        if ($standardDeviation < 1000 && $averageRpm > 1500) {
+            $data[] = [
+                'lng' => $lastEntry['lng'],
+                'lat' => $lastEntry['lat'],
+                'timestamp' => $lastEntry['timestamp'],
+                'points' => 10,
+                'violations' => [],
+                'obedience' => [
+                    'id' => 'EQUAL_RPM_OBEDIENCE',
+                    'desc' => sprintf('You kept your RPM (%.2f RPM) for a while, good job!', $averageRpm)
+                ]
+            ];
+        }
+    }
 
     return (new \Symfony\Component\HttpFoundation\JsonResponse($data))
         ->setEncodingOptions(
             \Symfony\Component\HttpFoundation\JsonResponse::DEFAULT_ENCODING_OPTIONS | JSON_PRETTY_PRINT
         );
 });
-// ... definitions
 
 $app->run();
