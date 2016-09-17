da6979bc944ccbd9fe3ee4d27bd7ec2b5371d71c
damian <damianszkudlarek@gmail.com>
2016-09-17 16:35:37 +0200

    + fix app file

--- web/app.php
+++ web/app.php
@@ -16,25 +16,54 @@ function calculateMs($knots) {
     return (int)$knots/1000 * 0.514444;
 }
 
-function decodeValue($value) {
+function decodeInt($value) {
     return unpack("Nval", base64_decode($value));
+}
+
+function decodeString($value) {
+    return implode(array_map("chr", unpack('C*', base64_decode($value))));
 }
 
 function caclulateAcceleration($speedAtBegining, $speedAtEnd, $timeInterval) {
     return ($speedAtEnd-$speedAtBegining)/$timeInterval;
 }
 
+// Function to calculate square of value - mean
+function sd_square($x, $mean) { return pow($x - $mean,2); }
+
+// Function to calculate standard deviation (uses sd_square)
+function sd($array) {
+
+// square root of sum of squares devided by N-1
+    return sqrt(array_sum(array_map("sd_square", $array, array_fill(0,count($array), (array_sum($array) / count($array)) ) ) ) / (count($array)-1) );
+}
+
+
 $m = new MongoDB\Client("mongodb://localhost:27017");
+
+$app->get('/decode-int', function () {
+    return new \Symfony\Component\HttpFoundation\Response(implode(array_map("chr", unpack('C*', base64_decode("QjoxMzEwNzI=")))));
+});
 
 $app->get('/api', function() use ($app, $m) {
     $rows = [];
-    $howMany = rand(1,5);
+    $howMany = rand(1,50);
 
     while($howMany-- > 0) {
-        $rows[] = $m->data->rows->findOneAndUpdate(['sent' => ['$exists' => false]], ['$set' => ['sent' => true]]);
+        $rows[] = $m->data->rows->findOneAndUpdate(
+            ['sent' => ['$exists' => false]],
+            ['$set' => ['sent' => true]],
+            ['sort' => ['recorded_at' => 1]]
+        );
     }
 
     return new \Symfony\Component\HttpFoundation\JsonResponse($rows);
+});
+
+$app->get('/reset-api', function() use ($app, $m) {
+    $m->data->rows->updateMany([], ['$unset' => ['sent' => '']]);
+
+    return new \Symfony\Component\HttpFoundation\Response("OK");
 });
 
 $app->get('/trip', function () use ($app, $m) {
@@ -45,36 +74,38 @@ $app->get('/trip', function () use ($app, $m) {
     $response = $app->handle($subRequest, \Symfony\Component\HttpKernel\HttpKernelInterface::SUB_REQUEST, false);
     $responseContent = json_decode($response->getContent(), true);
 
+    $speedList = [];
     foreach ($responseContent as $row) {
         if (empty($row['loc'])) {
             continue;
         }
 
-        $points = 10;
+        $points = 0;
         $violations = [];
+        $obedience = [];
         if ($row['fields']['BEHAVE_ID']['b64_value'] === ACCELERATION_VIOLATION) {
-            $points -= 10;
-            $beginingSpeed = calculateKpH(decodeValue($row['fields']['BEHAVE_GPS_SPEED_BEGIN']['b64_value'])['val']);
-            $endingSpeed = calculateKpH(decodeValue($row['fields']['BEHAVE_GPS_SPEED_END']['b64_value'])['val']);
-            $duration = decodeValue($row['fields']['BEHAVE_ELAPSED']['b64_value'])['val']/1000;
-            $violations[] = [
-                'id' => 'LIGHT_SPEED_VIOLATION',
-                'desc' => sprintf('You accelerated from %d km/h to %d km/h in %.2f seconds!', $beginingSpeed, $endingSpeed, $duration)
+            $points += 10;
+            $beginingSpeed = calculateKpH(decodeInt($row['fields']['BEHAVE_GPS_SPEED_BEGIN']['b64_value'])['val']);
+            $endingSpeed = calculateKpH(decodeInt($row['fields']['BEHAVE_GPS_SPEED_END']['b64_value'])['val']);
+            $duration = decodeInt($row['fields']['BEHAVE_ELAPSED']['b64_value'])['val']/1000;
+            $obedience[] = [
+                'id' => 'LIGHT_SPEED_OBEDIENCE',
+                'desc' => sprintf('You accelerated from %d km/h to %d km/h in %.2f seconds, now try to keep the speed!', $beginingSpeed, $endingSpeed, $duration)
             ];
         }
 
         if ($row['fields']['BEHAVE_ID']['b64_value'] === BREAKING_VIOLATION) {
             $points -= 10;
-            $beginingSpeed = calculateKpH(decodeValue($row['fields']['BEHAVE_GPS_SPEED_BEGIN']['b64_value'])['val']);
-            $endingSpeed = calculateKpH(decodeValue($row['fields']['BEHAVE_GPS_SPEED_END']['b64_value'])['val']);
-            $duration = decodeValue($row['fields']['BEHAVE_ELAPSED']['b64_value'])['val']/1000;
+            $beginingSpeed = calculateKpH(decodeInt($row['fields']['BEHAVE_GPS_SPEED_BEGIN']['b64_value'])['val']);
+            $endingSpeed = calculateKpH(decodeInt($row['fields']['BEHAVE_GPS_SPEED_END']['b64_value'])['val']);
+            $duration = decodeInt($row['fields']['BEHAVE_ELAPSED']['b64_value'])['val']/1000;
             $violations[] = [
                 'id' => 'HARD_BREAKS_VIOLATION',
                 'desc' => sprintf('You decelerated from %d km/h to %d km/h in %.2f seconds!', $beginingSpeed, $endingSpeed, $duration)
             ];
         }
 
-        $rpm = decodeValue($row['fields']['MDI_OBD_RPM']['b64_value'])['val'];
+        $rpm = decodeInt($row['fields']['MDI_OBD_RPM']['b64_value'])['val'];
         if ($rpm > 2000) {
             $points -= 10;
             $violations[] = [
@@ -83,13 +114,16 @@ $app->get('/trip', function () use ($app, $m) {
             ];
         }
 
-        $speed = decodeValue($row['fields']['MDI_OBD_SPEED']['b64_value'])['val'];
-        if ($speed > 130) {
-            $points -= 10;
-            $violations[] = [
-                'id' => 'OVERSPEED_VIOLATION',
-                'desc' => sprintf('Hey Bandit, keep calm and slow down a little, you\'re not in a plane, %d km/h is too much', $speed)
-            ];
+        $speed = decodeInt($row['fields']['MDI_OBD_SPEED']['b64_value'])['val'];
+        if (!empty($speed)) {
+            $speedList[] = $speed;
+            if ($speed > 130) {
+                $points -= 10;
+                $violations[] = [
+                    'id' => 'OVERSPEED_VIOLATION',
+                    'desc' => sprintf('Hey Bandit, keep calm and slow down a little, you\'re not in a plane, %d km/h is too much', $speed)
+                ];
+            }
         }
 
         $data[] = [
@@ -97,8 +131,28 @@ $app->get('/trip', function () use ($app, $m) {
             'lat' => $row['loc'][1],
             'timestamp' => strtotime($row['recorded_at']),
             'points' => $points,
-            'violations' => $violations
+            'violations' => $violations,
+            'obedience' => $obedience
         ];
+    }
+
+    if (count($speedList) > 5) {
+        $standardDeviation = sd($speedList);
+        $lastEntry = end($speedList);
+
+        if ($standardDeviation < 10) {
+            $data[] = [
+                'lng' => $lastEntry['lng'],
+                'lat' => $lastEntry['lat'],
+                'timestamp' => $lastEntry['timestampt'],
+                'points' => 10,
+                'violations' => [],
+                'obedience' => [
+                    'id' => 'EQUAL_SPEED_OBEDIENCE',
+                    'desc' => 'You kept your speed for a while, good job!'
+                ]
+            ];
+        }
     }
 
     return (new \Symfony\Component\HttpFoundation\JsonResponse($data))
