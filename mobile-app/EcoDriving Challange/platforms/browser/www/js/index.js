64d425f811f524d97b95ab422c85208934ba2620
diabelek <m.strozyk@udp.pl>
2016-09-17 13:05:18 +0200

    mobile app jQuery Mobile

--- mobile-app/EcoDriving Challange/platforms/browser/www/js/index.js
+++ mobile-app/EcoDriving Challange/platforms/browser/www/js/index.js
@@ -10,7 +10,7 @@ var app = {
         app.updateState();
     },
     updateState: function() {
-        var $welcomeScreen = $('.welcomeScrren');
+        var $welcomeScreen = $('#homePage');
         var $lvls = $welcomeScreen.find('.lECOvel');
         $.getJSON('http://172.31.2.19:8000/test.json', function(data) {
            if (data.currentLevel) {
@@ -38,3 +38,66 @@ var app = {
         });
     }
 };
+
+$(document).on("pageshow","#map",function(){ 
+    if (!$(this).hasClass('initialized')) {
+        $(this).addClass('initialized');
+        var map = initialize();
+
+        $.getJSON('http://172.31.2.19:8000/index.php/trip', function(data) {
+            poly = new google.maps.Polyline({
+                strokeColor: '#000000',
+                strokeOpacity: 1.0,
+                strokeWeight: 3
+            });
+
+            var path = poly.getPath();
+            var bounds = new google.maps.LatLngBounds();
+            var position = null;
+
+            for (var i = 0; i < data.length; i++) {
+                position = new google.maps.LatLng(data[i].lat,data[i].lng);
+                path.push(position);
+                bounds.extend(position);
+
+                if (typeof data[i].violations !== 'undefined' && data[i].violations.length) {
+                    var infowindow = new google.maps.InfoWindow({
+                      content: data[i].violations[0].desc
+                    });
+
+                    var marker = new google.maps.Marker({
+                        position: position,
+                        map: map,
+                        title: data[i].violations[0].desc
+                    });
+
+                    marker.addListener('click', function() {
+                      infowindow.open(map, marker);
+                    });
+                    }
+            }
+
+            poly.setMap(map);
+            map.fitBounds(bounds);
+        });
+    }
+});
+
+function initialize()
+{
+    var map = showMap();
+    return map;
+}
+
+function showMap()
+{
+    var mapOptions = {
+        zoom: 14,
+        center: new google.maps.LatLng(47.389822, 8.515603),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+    return map;
+}
